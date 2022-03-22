8fa7bfd7dc5777f10ecad994f0d5758ef9799027
Fernando <fherrera2018@gmail.com>
2022-03-22 01:38:31 -0700

    Updated second paragraph of about.

--- src/Endpoint/About.jsx
+++ src/Endpoint/About.jsx
@@ -30,7 +30,7 @@ function About(props) {
 				My name is Fernando Herrera-Gomez. I was born in Fresno, CA. My folks moved a lot to follow their seasonal work. Eventually we settled in Bakersfield CA. Going into <a href="https://www.csub.edu/">California State University Bakersfield</a>, I had it as my goal to eventually become a lawyer. In order to fulfill this goal, I studied and eventually graduated as the outstanding graduate in Philosophy for 2014.
 			</Typography>
 			<Typography paragraph>
-				I met good classmates and even better professors. One classmate turned me on to a <a href="https://law.ucla.edu/life-ucla-law/diversity-inclusion/outreach">UCLA Law Fellows</a> program where I applied to and eventually was a part of and a professor helped me land an internship in a law firm in Bakersfield. Attending this internship was a pivotal part of my life.
+				I met good classmates and even better professors. One classmate turned me on to a <a href="https://law.ucla.edu/life-ucla-law/diversity-inclusion/outreach">UCLA Law Fellows</a> program where I applied to and eventually was a part of. Soon after the program, a professor helped me land an internship in a law firm in Bakersfield. Attending this internship was a pivotal part of my life.
 			</Typography>
 			<Typography paragraph>
 				After some time with the internship, I started to have doubts as to my motivation to become a lawyer. After much introspection, I decided to no longer pursue a career in Law. Cut to 2016, I saw myself attending CSUB once more, but this time, as a computer science student! I knew that after my first year, programming was what I wanted to devote my life to.
