b53e38a396069b82be60d706dcc5d8617fffa814
Fernando <fherrera2018@gmail.com>
2022-07-03 15:38:59 -0700

    Temporarily removing bootstrap css to see size difference at build time.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {
     BrowserRouter
 } from 'react-router-dom';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
     <BrowserRouter >
