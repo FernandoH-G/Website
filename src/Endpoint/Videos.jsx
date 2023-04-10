76917a6f08b0f96b1e1d103c739e8e1ae5c5adaf
Fernando <fherrera2018@gmail.com>
2023-04-09 23:44:01 -0700

    Better version of videos in mobile mode. Still an issue in desktop mode.

--- src/Endpoint/Videos.jsx
+++ src/Endpoint/Videos.jsx
@@ -3,8 +3,9 @@ import { useEffect, useState } from "react"
 import Loading from "../Component/Loading"
 
 // External Imports
-import Container from "react-bootstrap/Container"
-// import YouTube from 'react-youtube';
+// import Container from "react-bootstrap/Container"
+import YouTube from 'react-youtube';
+import { Container, Grid } from "@mui/material"
 
 const Videos = (props) => {
   const { setHeaderMessage } = props
@@ -13,9 +14,9 @@ const Videos = (props) => {
   const title = "Videos"
   const message = "Recent uploads."
 
-  function onReady() {
+  // function onReady() {
 
-  }
+  // }
 
   useEffect(() => {
     setHeaderMessage({ title: title, subtitle: message })
@@ -24,8 +25,8 @@ const Videos = (props) => {
   useEffect(() => {
     async function getVideos() {
       // DONT LEAVE THESE HERE. PUT IT IN AN ENVIRONMENT VARIABLE!
-      const apiToken = ``
-      const playlistId = ``
+      const apiToken = process.env.REACT_APP_YOUTUBE_API_KEY
+      const playlistId = process.env.REACT_APP_PLAYLIST_ID
       const playlistItemsUrl = `playlistItems?part=snippet%2CcontentDetails&playlistId=${playlistId}&key=${apiToken}`
       const url = `https://youtube.googleapis.com/youtube/v3/${playlistItemsUrl}`
       const resp = await fetch(url)
@@ -41,38 +42,36 @@ const Videos = (props) => {
       }
       return videoIds
     }
-    // getVideos()
-    //   .then(result => {
-    //     setVideoIds(result)
-    //   })
-    //   .catch((err) => {
-    //     console.error(err)
-    //   })
+    getVideos()
+      .then(result => {
+        setVideoIds(result)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
 
   }, [])
   return (
-    <Container>
-      <div 
-      style={{display:"flex", flexWrap:"wrap"}}
-      >
+    // Deal with the left and right margin issue later!
+    <Grid container >
       {
-        // videoIds
-        //   ?
-        //   videoIds.map((videoId) => {
-        //     return (
-        //       <YouTube
-        //         key={videoId}
-        //         videoId={videoId}
-        //         onReady={onReady}
-        //         opts={{width: "200px", height: "200px"}}
-        //       />
-        //     )
-        //   })
-        //   :
-        //   <Loading />
+        videoIds
+          ?
+          videoIds.map((videoId) => {
+            return (
+              <Grid item xs={12} md={4} >
+                <YouTube
+                  key={videoId}
+                  videoId={videoId}
+                  opts={{ width: "100%", height: "100%" }}
+                />
+              </Grid>
+            )
+          })
+          :
+          <Loading />
       }
-      </div>
-    </Container>
+    </Grid>
   )
 
 }
